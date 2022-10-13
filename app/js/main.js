9e01f639a60c71341334c4f7edd40f1961417c8f
sergey-murzak <murzak301@gmail.com>
2022-10-14 04:19:40 +0600

    finish main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,11 @@
 // Smooth scroll https://github.com/cferdinandi/smooth-scroll
 // new SmoothScroll('a[href*="#"]')
+// document.addEventListener("DOMContentLoaded",()=>{
+//     let priceItemTitle = document.querySelectorAll('.price__item__title');
+//     priceItemTitle.forEach((e)=>{
+        
+//         if(e.innerText.length >= 35){
+//             e.style.lineHeight = '20px'
+//         }
+//     })
+// })
